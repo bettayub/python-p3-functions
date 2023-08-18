4db8e0224a6e28f66de53132c3902ed24c886cbf
bettayub <bettayub19@gmail.com>
2023-08-18 13:43:17 +0300

    completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
+
+# Testing the functions
+greet_programmer()
+greet("Alice")
+greet_with_default()
+greet_with_default("Bob")
+result = add(5, 3)
+print("Sum:", result)
+half_value = halve(10)
+print("Half:", half_value)
